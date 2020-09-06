f9ca353950487e08e104292f59209aaa85127c98
abutler911 <abutler911@gmail.com>
2020-09-06 16:30:38 -0600

    Update files and styles

--- client/client.js
+++ client/client.js
@@ -1,3 +1,20 @@
 console.log('Hello world');
 
 const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const formData = new FormData(form);
+	const name = formData.get('name');
+	const content = formData.get('content');
+	const yip = {
+		name,
+		content
+	};
+	console.log(yip);
+	form.style.display = 'none';
+	loadingElement.style.display = '';
+});
