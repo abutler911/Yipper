a522662500377237e5b6fa07251986f91024fe93
abutler911 <abutler911@gmail.com>
2020-09-06 17:03:25 -0600

    NPM Init

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {});
+
+app.listen(5000, () => {
+	console.log('Listening on http://localhost:5000');
+});
